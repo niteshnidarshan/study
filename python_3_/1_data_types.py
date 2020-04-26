47483278164d4baf51ea00ca4b3f94185cc9de89
Nitesh Nidarshan <niteshfromkatihar@gmail.com>
2020-04-26 07:49:10 +0530

    Add files via upload

---
+++ python_3_/1_data_types.py
@@ -0,0 +1,73 @@
+print("Jai Ram G Ki!")
+print("Hello World!")
+
+#Single line comment
+"""
+multi
+line
+comment
+"""
+
+#Variables
+#----------------
+rent = 1220
+gas = 202.5
+groceries = 305.6
+
+print(rent)
+
+total = rent+gas+groceries
+
+print(total)
+
+rent = 1400
+
+item1 = "rent"
+item2 = "gas"
+item3 = "groceries"
+
+print("Expense items : ", item1, item2, item3)
+
+
+# numbers and calculation
+# two types of numbers : intergers & fractions
+
+add = 12+34
+print(add);
+
+reminder = 11%2
+print(reminder)
+
+power = 3**2
+print(power)
+
+nyc_bal = 188
+bal_pitt = 247
+total_distance= nyc_bal+bal_pitt
+print("Total distance = ",total_distance);
+
+miles_per_hour = 65
+time = total_distance/miles_per_hour
+print("Total time taken in journey = ", time) # By default it shows 16 decimal points
+
+#To round up decimal points
+print("Total time taken in journey = ", round(time,2)) #To round up with two decimal points
+
+print("Total time taken in journey = ", round(time,4)) #To round up with four decimal points
+
+print(10+2*3)#BODMAS
+
+print((10+2)*3) #Force calculation
+
+print(6-5.7)#without round of by default
+
+print(round(6-5.7,2))#with accuracy
+
+
+
+
+
+
+
+
+
